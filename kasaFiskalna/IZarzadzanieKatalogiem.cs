8d4ee0549713f25b22ad42f9907aff252322eff4
eryk1995 <chiracle@gmail.com>
2016-11-17 15:12:16 +0100

    Add files via upload
    folder nie chcial sie zmienic na nazwe biblioteka

---
+++ kasaFiskalna/IZarzadzanieKatalogiem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteka
+{
+    interface IZarzadzanieKatalogiem
+    {
+
+
+        void WyszukajPoTytule() //string
+        {
+
+        }
+        void WyszukajPoId() //int
+        {
+
+        }
+
+        void WypiszWszystko()
+        {
+
+        }
+    }
+}
